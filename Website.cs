e6b2f2563f143ca194f183f177169afd03752072
Mathieu Philippe <mathieuphilippe.dev@gmail.com>
2023-11-13 17:18:16 +0800

    Fix the path

--- Website.cs
+++ Website.cs
@@ -4,7 +4,7 @@
     .AddSourceFiles("../FlexMotion/Runtime/**/*.cs")
     .DeployToGitHubPages(
         "MoArtis",
-        "https://moartis.github.io/",
+        "https://moartis.github.io",
         Config.FromSetting<string>("GITHUB_TOKEN"),
         "main")
     .RunAsync();
