dfb715290746aab0fd0d22a21c39962e1205dd0f
Mathieu Philippe <mathieuphilippe.dev@gmail.com>
2023-11-13 19:16:29 +0800

    Try random things

--- Website.cs
+++ Website.cs
@@ -1,10 +1,10 @@
 ﻿return await Bootstrapper
     .Factory
     .CreateDocs(args)
-    .AddSourceFiles("../FlexMotion/Runtime/**/*.cs")
     .DeployToGitHubPagesBranch(
         "MoArtis",
         "https://moartis.github.io",
         Config.FromSetting<string>("GITHUB_TOKEN"),
         "main")
+    .AddSourceFiles("../FlexMotion/Runtime/**/*.cs")
     .RunAsync();
