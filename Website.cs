5d2a9fa77f3a24e8f580c10f89821c5986a7c504
Mathieu Philippe <mathieuphilippe.dev@gmail.com>
2023-11-13 16:24:40 +0800

    Add github pages to the bootstrapper

--- Website.cs
+++ Website.cs
@@ -2,4 +2,9 @@
     .Factory
     .CreateDocs(args)
     .AddSourceFiles("../FlexMotion/Runtime/**/*.cs")
+    .DeployToGitHubPages(
+        "MoArtis",
+        "https://moartis.github.io",
+        Config.FromSetting<string>("GITHUB_TOKEN"),
+        "main")
     .RunAsync();
