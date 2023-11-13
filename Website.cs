fda43f4e0ddb84564ea324f4821974b1d21ecbf5
Mathieu Philippe <mathieuphilippe.dev@gmail.com>
2023-11-13 19:32:37 +0800

    Give up on deploy

--- Website.cs
+++ Website.cs
@@ -1,10 +1,5 @@
 ﻿return await Bootstrapper
     .Factory
     .CreateDocs(args)
-    .DeployToGitHubPagesBranch(
-        "MoArtis",
-        "Unity-SV-FlexMotion-Website",
-        Config.FromSetting<string>("GITHUB_TOKEN"),
-        "main")
     .AddSourceFiles("../FlexMotion/Runtime/**/*.cs")
     .RunAsync();
