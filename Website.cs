0e7cde0e65c478bffa05d13769cb9482e20c3f22
Mathieu Philippe <mathieuphilippe.dev@gmail.com>
2023-11-20 16:20:29 +0800

    Remove content filenames optimization to fix the casing issues with api member pages

--- Website.cs
+++ Website.cs
@@ -3,7 +3,8 @@
     .CreateDocs(args)
     .AddSourceFiles("../FlexMotion/Runtime/Core/**/*.cs")
     .AddSourceFiles("../FlexMotion/Runtime/Containers/**/*.cs")
-    .AddSetting("LinksUseHttps", "true")
+    .AddSetting(Keys.LinksUseHttps, "true")
     .AddSetting(Statiq.Markdown.MarkdownKeys.MarkdownExtensions, "Bootstrap")
     .AddShortcode<CalloutShortCode>("Callout")
+    .AddSetting(WebKeys.OptimizeContentFileNames, "false")
     .RunAsync();
